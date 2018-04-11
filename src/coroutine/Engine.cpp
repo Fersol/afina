470f49bd997f1d859dbef2c68ab8f60676f2bea7
Fersol <alex-2011.s@yandex.ru>
2018-04-11 10:39:39 +0300

    task6

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -7,13 +7,75 @@
 namespace Afina {
 namespace Coroutine {
 
-void Engine::Store(context &ctx) {}
+void Engine::Store(context &ctx) {
+	// variable on stack
+	char VarOnStack;
+    ctx.Low = &VarOnStack;
+    ctx.Hight = StackBottom;
+    size_t size = ctx.Hight - ctx.Low;
+    
+    //resize stack if needed
+    if (size > std::get<1>(ctx.Stack))
+    {
+        std::get<0>(ctx.Stack) = std::move(new char[size]);
+        std::get<1>(ctx.Stack) = size;
+    }
 
-void Engine::Restore(context &ctx) {}
+    memcpy(std::get<0>(ctx.Stack), ctx.Low, size);
+}
 
-void Engine::yield() {}
+void Engine::Restore(context &ctx) {
+	char VarOnStack;
+	// extend space
+    if (&VarOnStack >= ctx.Low) {
+    	Restore(ctx);
+    }
 
-void Engine::sched(void *routine_) {}
+    memcpy(ctx.Low, std::get<0>(ctx.Stack), std::get<1>(ctx.Stack));
+    // transfer itself
+    longjmp(ctx.Environment, 1);
+}
+
+void Engine::yield() {
+	context *new_routine = alive;
+
+	if (new_routine == nullptr) {
+    	return;
+    }
+
+    if (new_routine == cur_routine) {
+        if (new_routine->next != nullptr)
+        {
+            new_routine = new_routine->next;
+        }
+    }  
+
+    sched(new_routine);
+}
+
+void Engine::sched(void *routine_) {
+	if (routine_ == nullptr){
+		if(cur_routine != nullptr){
+			return;
+		}
+		else{
+			yield();
+		}
+	}
+	
+	context *ctx = (context*) routine_;
+
+    if (cur_routine != nullptr) {
+    	// will later be transferred
+    	if (setjmp(cur_routine->Environment) != 0) {
+    		return;
+    	}
+    	Store(*cur_routine);
+    }
+
+    cur_routine = ctx;
+    Restore(*cur_routine);
+}
 
 } // namespace Coroutine
 } // namespace Afina
